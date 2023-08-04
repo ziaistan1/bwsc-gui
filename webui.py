1475aa11dea74a35462dc349396f0898c2916a9d
ziaistan1 <99372296+ziaistan1@users.noreply.github.com>
2023-08-04 16:39:15 +0500

    webui.py

--- webui.py
+++ webui.py
@@ -453,7 +453,7 @@ while run_server:
 
         restart_server = False
         try:
-            barkgui.queue().launch(share=True, inbrowser=autolaunch, server_name=server_name, server_port=server_port, share=settings.server_share, prevent_thread_lock=True)
+            barkgui.queue().launch(share=True, inbrowser=autolaunch, server_name=server_name, server_port=server_port, prevent_thread_lock=True)
         except:
             restart_server = True
             run_server = False
